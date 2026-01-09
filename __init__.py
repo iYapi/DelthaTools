76c496f0c02a85e568d2827deed7c41c076b7d8c
MrYapikZ <yafialhaidar18@gmail.com>
2026-01-09 15:56:13 +0700

    release: v0.2.0

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "ExToolbox",
     "description": "Tools for Blender",
     "author": "MrYapikZ",
-    "version": (0, 1, 19),
+    "version": (0, 2, 0),
     "blender": (4, 5, 0),
 }
 
